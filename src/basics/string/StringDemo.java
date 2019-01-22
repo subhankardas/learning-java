af18bb939134bf3bcc4c16530941fea3398ef852
Subhankar Das <subhankardas831@gmail.com>
2019-01-22 18:26:31 +0530

    Added program to demostrate String and StringTokenizer classes

---
+++ src/basics/string/StringDemo.java
@@ -0,0 +1,60 @@
+package basics.string;
+
+import java.util.StringTokenizer;
+
+public class StringDemo {
+	
+	public static void main(String args[]) {
+		
+		byte[] data = {65, 66, 67, 88, 89, 90};
+		char[] chars = {'H', 'E', 'Y'};
+		
+		// Create string from byte[] i.e ASCII values
+		String str1 = new String(data);
+		
+		// Create string from char[]
+		String str2 = new String(chars);
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println("---------------------------");
+		
+		StringBuffer strBuffer = new StringBuffer("This is a long text!");
+		
+		System.out.println("Value: " + strBuffer);
+		System.out.println("Length: " + strBuffer.length());
+		System.out.println("Capacity: " + strBuffer.capacity());
+		System.out.println("---------------------------");
+		
+		String str3 = new String(strBuffer);
+		
+		System.out.println("Value: " + str3);
+		System.out.println("Length: " + str3.length());
+		System.out.println("---------------------------");
+		
+		// Tokenizing string
+		StringTokenizer strTokens = new StringTokenizer(str3);
+		while (strTokens.hasMoreTokens()) {
+			System.out.print("[" + strTokens.nextToken() + "] ");
+		}
+		
+		System.out.println();
+		
+		// Tokenizing string with delimeter
+		strTokens = new StringTokenizer("Some-weird-text-format-extra text with :)", "-");
+		strBuffer = new StringBuffer();
+		
+		while (strTokens.hasMoreTokens()) {
+			String value = strTokens.nextToken();
+			
+			System.out.print("[" + value + "] ");
+			strBuffer.append(value);
+		}
+		
+		System.out.println();
+		System.out.println("---------------------------");
+		System.out.println(strBuffer);
+		
+	}
+	
+}
