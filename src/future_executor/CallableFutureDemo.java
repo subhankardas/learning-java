40265e16be5262636f29d126edc8014ca847d1aa
Subhankar Das <subhankardas831@gmail.com>
2024-01-31 12:09:18 +0530

    :thread: Added future and executor service demo

---
+++ src/future_executor/CallableFutureDemo.java
@@ -0,0 +1,52 @@
+package future_executor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+// --- 1. Create a Callable implementation --- //
+class MyTask implements Callable<Integer> {
+
+    private String input;
+
+    public MyTask(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        // Simulate a long-running task with random delay
+        Thread.sleep((long) (Math.random() * 2000 + 500));
+        System.out.println("Task completed in thread: " + Thread.currentThread().getName() + " for input: " + input);
+        return Integer.valueOf(input.length());
+    }
+
+}
+
+public class CallableFutureDemo {
+
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // 2. Create instances of tasks
+        MyTask task1 = new MyTask("Hello");
+        MyTask task2 = new MyTask("Hi");
+        MyTask task3 = new MyTask("World!!");
+
+        // 3. Submit tasks to executor
+        Future<Integer> result1 = executor.submit(task1);
+        Future<Integer> result2 = executor.submit(task2);
+        Future<Integer> result3 = executor.submit(task3);
+
+        // 4. Wait for results
+        System.out.println(result1.get());
+        System.out.println(result2.get());
+        System.out.println(result3.get());
+
+        // 5. Shutdown executor service
+        executor.shutdown();
+    }
+
+}
---
+++ src/future_executor/SimpleFutureDemo.java
@@ -0,0 +1,46 @@
+package future_executor;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class SimpleFutureDemo {
+
+    // 1. Create an ExecutorService instance (with a single thread).
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    // 2. Return a Future instance from a Callable implementation.
+    public static Future<String> square(int num) {
+        return executor.submit(() -> { // Implement call method using lambda
+            Thread.sleep(1000l);
+            return "Result: " + (num * num);
+        });
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // -------- CREATE AND CONSUME A FUTURE TASK -------- //
+        Future<String> futureResult1 = square(3);
+        while (!futureResult1.isDone()) {
+            System.out.println("Waiting for square task to process...");
+            Thread.sleep(300);
+        }
+        String result1 = futureResult1.get();
+        System.out.println(result1);
+
+        // -------- CANCEL A FUTURE TASK -------- //
+        Future<String> futureResult2 = square(4);
+        futureResult2.cancel(true);
+
+        try { // To get result of cancelled task
+            futureResult2.get(); // Will throw CancellationException
+        } catch (CancellationException e) {
+            System.out.println("CancellationException");
+        }
+
+        // -------- SHUTDOWN THE EXECUTOR -------- //
+        executor.shutdown();
+    }
+
+}
